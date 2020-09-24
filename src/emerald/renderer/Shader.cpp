dc1f5798644d5949757a2d9f309a6cd00ffe1d8c
Daniel Warren <dpwarren455@gmail.com>
2020-09-23 20:50:42 -0700

    Add support for shader files

--- src/emerald/renderer/Shader.cpp
+++ src/emerald/renderer/Shader.cpp
@@ -6,6 +6,18 @@
 
 namespace Emerald {
 
+	Ref<Shader> Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None: EM_CORE_ASSERT(false, "RendererAPI::None is not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL: return CreateRef<OpenGLShader>(filepath);
+		}
+
+		EM_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<Shader> Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
